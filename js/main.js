ac5282680ca81cb905f52a9298d11eb432fa43e0
Unknown <kawser@designed.org>
2019-07-07 19:28:37 +0600

    improve code base

--- js/main.js
+++ js/main.js
@@ -1,47 +1,54 @@
 let onOffButton = $('#on_off_key');
 let keyState;
 
-var onTheButton = function(){
-        onOffButton.html(" ON ");
-    };
+var onTheButton = function() {
+    onOffButton.html(" ON ");
+};
 
-    var offTheButton = function(){
-        onOffButton.html("OFF");
-    };
+var offTheButton = function() {
+    onOffButton.html("OFF");
+};
 
-    var switchLanguage = function(isON){
-        $('textarea, input').avro({'bangla':isON});
-    };
-
-    var buttonSwitcher = function(isON){
-        if(isON){
-            onTheButton();
-        }else{
-            offTheButton();
-        }
-    }
-
-    onOffButton.on('click',function(){
-        chrome.storage.sync.get('isON', function(data) {
-            buttonSwitcher(!data.isON);
-            switchLanguage(!data.isON);
-            chrome.storage.sync.set({isON: !data.isON},function(){});
-            location.reload(true);
-        });
-    });
-
-window.onload = function() {
-    chrome.storage.sync.get('isON', function(data) {
-        if(data.isON === undefined){
-            keyState = false;
-            offTheButton();
-        }
-        else{
-            keyState = data.isON;
-            buttonSwitcher(data.isON); 
-        }
-        switchLanguage(keyState);
+var switchLanguage = function(isON) {
+    $('textarea, input').avro({
+        'bangla': isON
     });
 };
 
+var buttonSwitcher = function(isON) {
+    if (isON) {
+        onTheButton();
+    } else {
+        offTheButton();
+    }
+};
 
+var _action = function(keyState){
+    buttonSwitcher(keyState);
+    switchLanguage(keyState);
+    chrome.storage.sync.set({
+        isON: keyState
+    }, function() {});
+};
+
+onOffButton.on('click', function() {
+    chrome.storage.sync.get('isON', function(data) {
+        keyState = !data.isON;
+        _action(keyState);
+        location.reload(true);
+    });
+    
+    location.reload(true);
+});
+
+window.onload = function() {
+    chrome.storage.sync.get('isON', function(data) {
+        if (data.isON === undefined) {
+            keyState = false;
+        } else {
+            keyState = data.isON;
+        }
+        _action(keyState);
+        console.log('keyState:' + keyState);
+    });
+};
