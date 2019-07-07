34382544afd1f713a3faa183913c5e4bdb542829
Unknown <kawser@designed.org>
2019-07-08 01:26:13 +0600

    runtime text save feature added

--- js/main.js
+++ js/main.js
@@ -24,36 +24,53 @@ var buttonSwitcher = function(isON) {
 function action(keyState) {
     buttonSwitcher(keyState);
     switchLanguage(keyState);
-    browser.storage.local.set({data: {isON: keyState}});
+    browser.storage.local.set({isON: keyState});
 }
 
 function onError(error) {
     console.log(`Error Controller: ${error}`);
 }
 
+var timeoutId;
+let textArea = $('#emergency_input');
+textArea.on('input propertychange change', function() {
+clearTimeout(timeoutId);
+    timeoutId = setTimeout(function() {   
+        saveToDB(textArea.val());
+    }, 1000);
+});
+
+function saveToDB(textMsg)
+{
+    browser.storage.local.set({msg: textMsg});  
+}
+
 onOffButton.on('click', function() {
-    let gettingData = browser.storage.local.get("data");
-    gettingData.then(function(item) {
-        console.log("onclick Call");
-        console.log("onclick value : " + item.data.isON);
-        console.log(item.data.isON);
-        keyState = !item.data.isON;
+    let gettingData = browser.storage.local.get("isON");
+    gettingData.then(function(data) {
+        console.log(data.isON);
+        keyState = !data.isON;
         action(keyState);
         location.reload(true);
     }, onError);
 });
 
 window.onload = function() {
-    let gettingData = browser.storage.local.get("data");
+    let gettingData = browser.storage.local.get();
     gettingData.then(function(item) {
-        console.log("Init Call");
-        //console.log("init value : " + item.data.isON);
         if (item.data === undefined) {
             keyState = false;
         } else {
-            keyState = item.data.isON;
+            keyState = item.isON;
         }
         action(keyState);
+
+        if(item.msg == undefined){
+            //do nothing
+        }else{
+            textArea.val(item.msg);
+        }
+
     }, onError);
 };
 
