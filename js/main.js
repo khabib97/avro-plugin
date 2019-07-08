980531b32cda2356fccf05aa71cea3c374cf88bb
Unknown <kawser@designed.org>
2019-07-08 15:22:35 +0600

    stable firefox version complete

--- js/main.js
+++ js/main.js
@@ -1,77 +1,84 @@
-let onOffButton = $('#on_off_key');
+let checkBox = $("#on_off");
 let keyState;
 
+var timeoutId;
+let textArea = $("#emergency_input");
+
 var onTheButton = function() {
-    onOffButton.html(" ON ");
+  checkBox.prop("checked", true);
 };
 
 var offTheButton = function() {
-    onOffButton.html("OFF");
+  checkBox.prop("checked", false);
 };
 
 var switchLanguage = function(isON) {
-    $('textarea, input').avro({'bangla': isON});
+  $("textarea, input").avro({ bangla: isON });
 };
 
 var buttonSwitcher = function(isON) {
-    if (isON) {
-        onTheButton();
-    } else {
-        offTheButton();
-    }
-}
+  if (isON) {
+    onTheButton();
+  } else {
+    offTheButton();
+  }
+};
 
-function action(keyState) {
-    buttonSwitcher(keyState);
-    switchLanguage(keyState);
-    browser.storage.local.set({isON: keyState});
-}
+var iconSwither = function(isON) {
+  if (isON) {
+    browser.browserAction.setIcon({ path: "img/avro.png" });
+  } else {
+    browser.browserAction.setIcon({ path: "img/avro_disable.png" });
+  }
+};
 
-function onError(error) {
-    console.log(`Error Controller: ${error}`);
-}
+var action = function(keyState) {
+  console.log("action 1: " + keyState);
+  buttonSwitcher(keyState);
+  switchLanguage(keyState);
+  iconSwither(keyState);
+  console.log("action 2: " + keyState);
+  browser.storage.local.set({ isON: keyState });
+};
 
-var timeoutId;
-let textArea = $('#emergency_input');
-textArea.on('input propertychange change', function() {
-clearTimeout(timeoutId);
-    timeoutId = setTimeout(function() {   
-        saveToDB(textArea.val());
-    }, 1000);
+var onError = function(error) {
+  console.log(`Error Controller: ${error}`);
+};
+
+var saveToDB = function(textMsg) {
+  browser.storage.local.set({ msg: textMsg });
+};
+
+textArea.on("input propertychange change", function() {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(function() {
+    saveToDB(textArea.val());
+  }, 1000);
 });
 
-function saveToDB(textMsg)
-{
-    browser.storage.local.set({msg: textMsg});  
-}
-
-onOffButton.on('click', function() {
-    let gettingData = browser.storage.local.get("isON");
-    gettingData.then(function(data) {
-        console.log(data.isON);
-        keyState = !data.isON;
-        action(keyState);
-        location.reload(true);
-    }, onError);
+checkBox.change(function() {
+  let gettingData = browser.storage.local.get("isON");
+  gettingData.then(function(data) {
+    console.log("click: " + data.isON);
+    keyState = !data.isON;
+    action(keyState);
+    location.reload(true);
+  }, onError);
 });
 
 window.onload = function() {
-    let gettingData = browser.storage.local.get();
-    gettingData.then(function(item) {
-        if (item.data === undefined) {
-            keyState = false;
-        } else {
-            keyState = item.isON;
-        }
-        action(keyState);
+  let gettingData = browser.storage.local.get();
+  gettingData.then(function(item) {
+    if (item.isON === undefined) {
+      keyState = false;
+    } else {
+      keyState = item.isON;
+    }
 
-        if(item.msg == undefined){
-            //do nothing
-        }else{
-            textArea.val(item.msg);
-        }
-
-    }, onError);
+    if (item.msg === undefined) {
+    } else {
+      textArea.val(item.msg);
+    }
+    action(keyState);
+  }, onError);
 };
-
-
